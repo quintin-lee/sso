37e440bc04d56ab43e5e789568dc03bb4fdd70f3
quintin <2449164582@qq.com>
2026-06-17 07:18:13 +0800

    feat(frontend): 🍞 setup PrimeVue ToastService

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import Toast from 'primevue/toast';
+</script>
+
 <template>
+  <Toast />
   <router-view />
 </template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
 import 'primeicons/primeicons.css'
 import './style.css'
 import App from './App.vue'
@@ -7,5 +8,6 @@ import router from './router'
 
 const app = createApp(App)
 app.use(PrimeVue)
+app.use(ToastService)
 app.use(router)
 app.mount('#app')
